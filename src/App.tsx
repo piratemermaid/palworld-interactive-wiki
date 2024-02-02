31d2786e3ab037fd70be73132d5eaffec7dbdd6a
Ariel <thepiratemermaid4@gmail.com>
2024-02-01 17:57:00 -0700

    Add MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { ThemeProvider, createTheme } from '@mui/material';
+import { indigo, red } from '@mui/material/colors';
+
 import PalWorkSkills from './components/PalWorkSkills';
 import './App.css';
 
+const primaryColor = indigo[500];
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: red[400],
+    },
+  },
+  typography: {
+    h2: { fontSize: 30 },
+    h6: { color: primaryColor },
+  },
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <PalWorkSkills />
-    </>
+    </ThemeProvider>
   );
 }
 
