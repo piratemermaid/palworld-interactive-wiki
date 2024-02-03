157626f50763eef033a1ebfe592cc37f6fbade52
Ariel <thepiratemermaid4@gmail.com>
2024-02-03 15:25:36 -0700

    Add user pal props to store

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import type { PalName } from './types/pal';
 
 type State = {
   darkMode: boolean;
   toggleDarkMode: () => void;
+
+  userPals: PalName[];
+  addUserPal: (palName: PalName) => void;
+  removeUserPal: (palName: PalName) => void;
 };
 
 export const useStore = create<State, [['zustand/persist', State]]>(
@@ -13,6 +18,16 @@ export const useStore = create<State, [['zustand/persist', State]]>(
       toggleDarkMode: () => {
         const darkMode = get().darkMode;
         return set({ darkMode: !darkMode });
+      },
+
+      userPals: [],
+      addUserPal: (palName) => {
+        // const darkMode = get().darkMode;
+        // return set({ darkMode: !darkMode });
+      },
+      removeUserPal: (palName) => {
+        // const darkMode = get().darkMode;
+        // return set({ darkMode: !darkMode });
       },
     }),
     {
