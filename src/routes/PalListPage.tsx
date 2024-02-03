39d8ccd9a3057998e08a4d2b886f945903fa97ed
Ariel <thepiratemermaid4@gmail.com>
2024-02-03 15:23:26 -0700

    Display current pal data on new pal list page

--- src/routes/PalListPage.tsx
+++ src/routes/PalListPage.tsx
@@ -1,3 +1,30 @@
+import { Card, Grid, Typography } from '@mui/material';
+import { map } from 'lodash';
+
+import { PAL_DATA } from '../data/pals';
+
 export default function PalListPage() {
-  return <h1>PAL LIST GOES HERE</h1>;
+  return (
+    <>
+      <Typography variant="h2">Pal List</Typography>
+      <Grid container spacing={2}>
+        {map(PAL_DATA, (palData, palName) => {
+          return (
+            <Grid item lg={3} md={4} sm={6} xs={12}>
+              <Card sx={{ p: 2 }}>
+                <Typography variant="h6">{palName}</Typography>
+                <Typography variant="body2">#{palData.paldeckNo}</Typography>
+                <Typography variant="body2" sx={{ textAlign: 'left' }}>
+                  Skill: TODO
+                </Typography>
+                <Typography variant="body2" sx={{ textAlign: 'left' }}>
+                  Drops: TODO
+                </Typography>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
 }
