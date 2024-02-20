dcf7556e06e9aad8b1cbbe004d7b3efb03ee75f3
piratemermaid <thepiratemermaid4@gmail.com>
2024-02-20 14:40:52 -0500

    Move My Pal card code into own component

--- src/routes/MyPalsPage.tsx
+++ src/routes/MyPalsPage.tsx
@@ -1,87 +1,26 @@
-import { Card, Checkbox, Grid, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
-import { useStore } from '../store';
-import { PAL_DATA, PAL_NAME_LIST } from '../data/pals';
+import { PAL_NAME_LIST } from '../data/pals';
+import MyPalCard from '../components/MyPalCard';
 
 export default function MyPalsPage() {
-  const userPals = useStore((store) => store.userPals);
-  const updateUserPals = useStore((store) => store.updateUserPals);
-  const userPalsCaughtTen = useStore((store) => store.userPalsCaughtTen);
-  const updateUserPalsCaughtTen = useStore(
-    (store) => store.updateUserPalsCaughtTen,
-  );
-
   return (
-    <>
-      <Typography variant="h2" sx={{ mb: 2 }}>
-        My Pals
-      </Typography>
-
-      <Grid container spacing={2}>
-        {PAL_NAME_LIST.map((palName) => {
-          const palData = PAL_DATA[palName];
-
-          const havePal = userPals.includes(palName);
-          const haveCaughtTen = userPalsCaughtTen.includes(palName);
-
-          return (
-            <Grid
-              item
-              key={palName}
-              lg={2}
-              md={3}
-              sm={6}
-              xs={12}
-              sx={{ cursor: 'pointer' }}
-            >
-              <Card sx={{ p: 1 }}>
-                <Grid
-                  container
-                  sx={{ textAlign: 'left' }}
-                  justifyContent="space-between"
-                >
-                  <Grid item onClick={() => updateUserPals(palName)}>
-                    <Grid container>
-                      <Grid item>
-                        <Checkbox checked={havePal} />{' '}
-                      </Grid>
-                      <Grid item sx={{ position: 'relative', top: 6 }}>
-                        <Typography
-                          variant="body2"
-                          color={havePal ? 'primary' : ''}
-                        >
-                          {palName}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          color={havePal ? 'primary' : ''}
-                          sx={{ fontSize: 12 }}
-                        >
-                          #{palData.paldeckNo}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-
-                  <Grid
-                    item
-                    sx={{ float: 'left', fontSize: 12 }}
-                    onClick={() => updateUserPalsCaughtTen(palName)}
-                  >
-                    <Checkbox
-                      checked={haveCaughtTen}
-                      size="small"
-                      sx={{ position: 'relative', bottom: 2 }}
-                      color="secondary"
-                    />
-                    10
-                  </Grid>
-                </Grid>
-              </Card>
-            </Grid>
-          );
-        })}
+    <Grid container direction="column">
+      <Grid item>
+        <Typography variant="h2" sx={{ mb: 2 }}>
+          My Pals
+        </Typography>
       </Grid>
-    </>
+
+      <Grid item>FILTERS</Grid>
+
+      <Grid item>
+        <Grid container spacing={2}>
+          {PAL_NAME_LIST.map((palName) => (
+            <MyPalCard key={palName} palName={palName} />
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
