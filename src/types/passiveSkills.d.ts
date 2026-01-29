10ee280247a2158b43b504a5f66d58eb1f9f099e
Ariel <thepiratemermaid4@gmail.com>
2026-01-28 19:47:01 -0700

    add traits and categories

--- src/types/passiveSkills.d.ts
+++ src/types/passiveSkills.d.ts
@@ -3,11 +3,14 @@ import { PASSIVE_SKILL_TYPES } from '@data';
 type PassiveSkillType = (typeof PASSIVE_SKILL_TYPES)[number];
 
 declare global {
+  type PassiveSkillCategory = 'Base' | 'Mount' | 'Hunger' | null;
+
   type PassiveSkill = {
     name: string;
     types: PassiveSkillType[];
     description: string;
-    tier: 1 | 2 | 3 | -1 | -2 | -3;
+    tier: 1 | 2 | 3 | -1 | -2 | -3 | 'Rainbow';
+    category?: PassiveSkillCategory;
   };
 }
 
